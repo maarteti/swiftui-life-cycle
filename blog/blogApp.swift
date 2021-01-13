48fa0db7e5d10d92a4885eb1ef8c46cb8a0c35b6
maarteti <maarteti@gmail.com>
2021-01-13 21:16:03 +0900

    Initial Commit

---
+++ blog/blogApp.swift
@@ -0,0 +1,17 @@
+//
+//  blogApp.swift
+//  blog
+//
+//  Created by albin.p on 2021/01/13.
+//
+
+import SwiftUI
+
+@main
+struct blogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
